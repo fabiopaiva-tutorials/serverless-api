39d6c2d6567b1d977fc09006f87d440f32472a8a
Fábio Paiva <fabio@skyworkz.nl>
2021-07-21 10:57:58 +0200

    chore: prettier fmt

--- handler.ts
+++ handler.ts
@@ -1,35 +1,35 @@
 import { APIGatewayEvent } from "aws-lambda";
 
 export const hello = async (event: APIGatewayEvent) => ({
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Hello",
-      },
-      null,
-      2
-    ),
-  });
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: "Hello",
+    },
+    null,
+    2
+  ),
+});
 
-  export const world = async (event: APIGatewayEvent) => ({
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "World",
-      },
-      null,
-      2
-    ),
-  });
+export const world = async (event: APIGatewayEvent) => ({
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: "World",
+    },
+    null,
+    2
+  ),
+});
 
-  export const post = async (event: APIGatewayEvent) => ({
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Post",
-        body: event.body,
-      },
-      null,
-      2
-    ),
-  });
+export const post = async (event: APIGatewayEvent) => ({
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: "Post",
+      body: event.body,
+    },
+    null,
+    2
+  ),
+});
